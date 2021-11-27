ef41a7765fb4307642b37d4d972e6dc411ffcf6f
Luis <Luis@LAPTOP-0EFHT9GS>
2021-11-27 14:24:21 -0500

    Implement iFrame in violence component

--- src/components/Violence/index.tsx
+++ src/components/Violence/index.tsx
@@ -1,6 +1,8 @@
 import { useRouter } from 'next/dist/client/router'
 import React, { FC, useContext } from 'react'
 import { ViolencesContext } from '../../context/ViolencesContext'
+import Iframe from 'react-iframe'
+import styles from './Violence.module.css'
 
 // Agregar la lista al context
 
@@ -9,24 +11,42 @@ type ViolenceProps = {
   title: string
   id: number
 }
-const Violence: FC<ViolenceProps> = ({src, title, id}) => {
+const Violence: FC<ViolenceProps> = ({ src, title, id }) => {
   const { setCurrentViolences } = useContext(ViolencesContext)
   const router = useRouter()
+
   const urlTitle = title.replaceAll(' ', '-')
+  const { embedVideo, violenceContainer, titleViolence } = styles
+
   const handler = (e: any) => {
     e.preventDefault()
-    router.push(`/compartir/${urlTitle}`)
-    if(setCurrentViolences) setCurrentViolences(`${id}`)
+    //router.push(`/compartir/${urlTitle}`)
+    router.push(`https://hackaton-3-gqktl.ondigitalocean.app/violences/${id}`)
+  
+    if (setCurrentViolences) setCurrentViolences(`${id}`)
   }
   return (
-    <div
-      onClick={handler} 
-    >
-      <img src={src} alt='Violencia' />
-      <h3>
-        {title}
-      </h3>
-    </div>
+    <>
+      <div
+        className={violenceContainer}
+      >
+        <Iframe
+          url="http://www.youtube.com/embed/jhgVu2lsi_k"
+          width="100%"
+          height="80%"
+          id="myId"
+          className={embedVideo}
+          position="relative"
+          onLoad={() => {
+            console.log('cargando');
+          }}
+        />
+        <h3 className={titleViolence} onClick={handler} >
+          {title}
+        </h3>
+      </div>
+      
+    </>
   )
 }
 
