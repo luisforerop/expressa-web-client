019b02aff054a08b9f609620075702a15495c15d
Luis <Luis@LAPTOP-0EFHT9GS>
2021-11-27 14:37:52 -0500

    Stylize violences list component

--- src/components/ViolencesList/index.tsx
+++ src/components/ViolencesList/index.tsx
@@ -1,16 +1,19 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import { tLevel } from '../../../types/global'
 import useGetViolencesList from '../../hooks/useGetViolencesList'
 import Violence from '../Violence'
+import styles from './ViolencesList.module.css'
+
 
 type ViolencesListProps = {
   level: tLevel
 }
 
-const ViolencesList: FC<ViolencesListProps> = ({level}) => {
+const ViolencesList: FC<ViolencesListProps> = ({ level }) => {
   const { isFetching, violencesList } = useGetViolencesList()
+  const { violencesListContainer } = styles
   return (
-    <>
+    <div className={violencesListContainer}>
       {violencesList?.map(violenceList => violenceList.level !== level ? null : (
         <Violence
           key={violenceList.id}
@@ -19,7 +22,7 @@ const ViolencesList: FC<ViolencesListProps> = ({level}) => {
           id={violenceList.id}
         />
       ))}
-    </>
+    </div>
   )
 }
 
