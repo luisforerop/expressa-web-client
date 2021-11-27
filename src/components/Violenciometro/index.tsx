4b1836896b76bb1b3a2595e5dc7cdda4012aac3e
Luis <Luis@LAPTOP-0EFHT9GS>
2021-11-27 14:25:52 -0500

    Implement filters in violenciometro

--- src/components/Violenciometro/index.tsx
+++ src/components/Violenciometro/index.tsx
@@ -1,21 +1,24 @@
 import { type } from 'os'
 import React, { FC } from 'react'
+import styles from './Violenciometro.module.css'
 
 type ViolenciometroProps = {
   setLevel: Function
 }
 
-const Violenciometro: FC<ViolenciometroProps> = ({setLevel}) => {
-  const levels = [ 'ALERTA', 'REACCIONA', 'URGENTE' ]
-
+const Violenciometro: FC<ViolenciometroProps> = ({ setLevel }) => {
+  const levels = ['ALERTA', 'REACCIONA', 'URGENTE']
+  const { button, violenciometerContainer,  } = styles
+  
   return (
-    <div>
+    <div className={violenciometerContainer}>
       {levels.map((level, index) => (
-        <button 
-          key={`${index}-${level}`} 
+        <button
+          key={`${index}-${level}`}
           onClick={() => {
             setLevel(level)
           }}
+          className={`${button} ${styles[`button${level}`]}`}
         >
           {level}
         </button>
