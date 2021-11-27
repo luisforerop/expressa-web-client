51b783cd9014d2d55da324da1bc02edd7dfbac06
Luis <Luis@LAPTOP-0EFHT9GS>
2021-11-27 00:43:31 -0500

    Init empty project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,13 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      Aquí va el header
+      <Component {...pageProps} />
+      Aquí va el footer
+    </>
+  )
 }
 
 export default MyApp
